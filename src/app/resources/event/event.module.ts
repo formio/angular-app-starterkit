a89b2f4ecd2206dc500880e0c3e5e67e8c08b7f7
Travis Tidwell <travis@form.io>
2017-03-25 10:02:22 -0500

    Show how nested resources can be created.

--- src/app/resources/event/event.module.ts
+++ src/app/resources/event/event.module.ts
@@ -1,15 +1,33 @@
 import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
 import { AppConfig } from '../../../config';
 import {
   FormioResource,
   FormioResourceRoutes,
   FormioResourceConfig,
-  FormioResourceService
+  FormioResourceService,
+  FormioResourceComponent
 } from 'ng2-formio/resource';
 
-let eventResourceRoutes: Routes = FormioResourceRoutes();
+// provide our own Event resource page.
+@Component({
+  styles: [
+    'ul.nav.nav-tabs { margin-bottom: 20px; }',
+    '.resource-back-icon { font-size: 1.2em; padding: 0.6em; }'
+  ],
+  template: require('./event.html')
+})
+export class EventResourceComponent extends FormioResourceComponent {
+  constructor(private service: FormioResourceService, private route: ActivatedRoute) {
+    super(service, route);
+  }
+}
+
+// add the Activity nested resource.
+let eventResourceRoutes: Routes = FormioResourceRoutes({
+  resource: EventResourceComponent
+});
 eventResourceRoutes[2].children.push({
   path: 'activity',
   loadChildren: 'es6-promise-loader?,[name]!../activity/activity.module#ActivityResource'
@@ -20,6 +38,9 @@ eventResourceRoutes[2].children.push({
     CommonModule,
     FormioResource,
     RouterModule.forChild(eventResourceRoutes)
+  ],
+  declarations: [
+    EventResourceComponent
   ],
   providers: [
     FormioResourceService,
